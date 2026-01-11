4cfa9c0aa3b841d2366a715fafb4b11e49d369b1
rounak <rounakgope50@gmail.com>
2026-01-11 16:53:47 +0530

    Restaurant model added

--- backend/src/main/java/com/fung/fungry/Model/Address.java
+++ backend/src/main/java/com/fung/fungry/Model/Address.java
@@ -32,6 +32,7 @@ public class Address {
     @OneToMany( mappedBy = "address",fetch = FetchType.LAZY)
     private List<Order> orders=new ArrayList<>();
 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
--- backend/src/main/java/com/fung/fungry/Model/Restaurant.java
+++ backend/src/main/java/com/fung/fungry/Model/Restaurant.java
@@ -1,4 +1,34 @@
 package com.fung.fungry.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurant")
+@NoArgsConstructor
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long restaurantId;
+
+    @Column(name = "restaurant_name",nullable = false)
+    private String name;
+
+    @Embedded
+    private RestaurantAddress address;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "restaurant",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<MenuItem> menuItems=new ArrayList<>();
+
+    @Column(name = "ratings")
+    private Integer rating ;
+
 }
