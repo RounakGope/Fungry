2c1589bb37dd966ec951ef2b45a8386cece23657
rounak <rounakgope50@gmail.com>
2026-01-11 13:22:43 +0530

    user model added

---
+++ backend/src/main/java/com/fung/fungry/Model/User.java
@@ -0,0 +1,45 @@
+package com.fung.fungry.Model;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(name = "user_Name", nullable = false)
+    private String userName;
+    @Column(name = "user_Password",nullable = false)//this is db level validation
+    private String userPasswordHash;
+    @Column(name = "role",nullable = false)
+    private String role;
+    @Column(name = "phone_Number",nullable = false)
+    private Long phoneNumber;
+    @OneToMany(
+            mappedBy = "user",//mappedBy must match the field name in the CHILD entity, not the table name.
+           cascade = CascadeType.ALL
+            ,fetch=FetchType.LAZY
+    )
+    private List<Address> addressList=new ArrayList<>();
+
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL
+            ,fetch = FetchType.LAZY
+    )
+    private List<Order> orderHistory=new ArrayList<>();
+    @CreationTimestamp
+    LocalDateTime createdAt;
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+}
