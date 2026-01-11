8e03b7f55cc7c96afd8059bc3a467c35f955bceb
rounak <rounakgope50@gmail.com>
2026-01-11 14:48:33 +0530

    address model added

--- backend/src/main/java/com/fung/fungry/Model/Order.java
+++ backend/src/main/java/com/fung/fungry/Model/Order.java
@@ -30,9 +30,11 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false)
     private  User user;
+
     @ManyToOne(fetch = FetchType.LAZY)
             @JoinColumn(name = "address_id",nullable = false)
    private Address address;
+
     private int expectedTimeInMinutes;
     @ManyToOne(fetch = FetchType.LAZY)
             @JoinColumn(name = "restaurant_id",nullable = false)
