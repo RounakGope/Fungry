6685747948014850e706d1b7a1bcb4e00b3ef1df
rounak <rounakgope50@gmail.com>
2026-01-11 14:48:33 +0530

    address model added

--- backend/src/main/java/com/fung/fungry/Model/Address.java
+++ backend/src/main/java/com/fung/fungry/Model/Address.java
@@ -1,4 +1,38 @@
 package com.fung.fungry.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "addresses")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @Column(name = "zipcode",nullable = false)
+    private Integer zipcode;
+
+    @Column(name = "house_number")
+    private int houseNumber;
+    @Column(name = "landmark")
+    private String landmark;
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany( mappedBy = "address",fetch = FetchType.LAZY)
+    private List<Order> orders=new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
 }
