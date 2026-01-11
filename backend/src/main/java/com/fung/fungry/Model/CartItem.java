537fedadf7d4a91281342e0da353775166c02852
rounak <rounakgope50@gmail.com>
2026-01-11 21:21:16 +0530

    Cart item added

--- backend/src/main/java/com/fung/fungry/Model/CartItem.java
+++ backend/src/main/java/com/fung/fungry/Model/CartItem.java
@@ -1,4 +1,29 @@
 package com.fung.fungry.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_item_id", nullable = false)
+    private MenuItem menuItem;
+
+    @Column(nullable = false)
+    private Integer quantity;
 }
