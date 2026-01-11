7f2c1ee29e1f3bde55fe9958aa9bdba66f05c767
rounak <rounakgope50@gmail.com>
2026-01-11 14:31:09 +0530

    order item model added

--- backend/src/main/java/com/fung/fungry/Model/OrderItem.java
+++ backend/src/main/java/com/fung/fungry/Model/OrderItem.java
@@ -1,4 +1,38 @@
 package com.fung.fungry.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "order_items")
+@NoArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @Column(name = "order_item_name",nullable = false)
+    private String orderItemName;
+
+    @Column(name = "quantity",nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price",nullable = false)
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_item_id")
+    private MenuItem menuItem;
+
+
+
 }
