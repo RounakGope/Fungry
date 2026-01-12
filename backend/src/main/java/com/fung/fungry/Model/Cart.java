829a4a932c5d27dd10eab5c83e3f2ef89fa38135
rounak <rounakgope50@gmail.com>
2026-01-12 21:57:47 +0530

    updated cart

--- backend/src/main/java/com/fung/fungry/Model/Cart.java
+++ backend/src/main/java/com/fung/fungry/Model/Cart.java
@@ -20,6 +20,12 @@ public class Cart {
     @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY,orphanRemoval = true,cascade = CascadeType.ALL)
     private List<CartItem> cartItems=new ArrayList<>();
 
+    @Column(name = "restaurant",nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "total_amt",nullable = false)
+    private Double totalAmt;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false,unique = true)
     private User user;
