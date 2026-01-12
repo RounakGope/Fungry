2aee15d304f621b1eea03fc8f5028dc4067a31e0
rounak <rounakgope50@gmail.com>
2026-01-12 22:44:19 +0530

    payment entity added

---
+++ backend/src/main/java/com/fung/fungry/Enums/PaymentMode.java
@@ -0,0 +1,5 @@
+package com.fung.fungry.Enums;
+
+public enum PaymentMode {
+    CASH,CARD,NET_BANKING,PAY_LATER,GIFT_CARD;
+}
---
+++ backend/src/main/java/com/fung/fungry/Model/Payment.java
@@ -0,0 +1,33 @@
+package com.fung.fungry.Model;
+
+import com.fung.fungry.Enums.PaymentMode;
+import com.fung.fungry.Enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMode paymentMode;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false,name = "order_id")
+    private Order order;
+
+
+
+
+}
