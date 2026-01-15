8429963b020de2030800c8df72d6faf4e16be02f
rounak <rounakgope50@gmail.com>
2026-01-15 22:45:09 +0530

    PaymentDTO added

--- backend/src/main/java/com/fung/fungry/ModelDTO/PaymentDTO.java
+++ backend/src/main/java/com/fung/fungry/ModelDTO/PaymentDTO.java
@@ -1,4 +1,20 @@
 package com.fung.fungry.ModelDTO;
 
+import com.fung.fungry.Enums.PaymentMode;
+import com.fung.fungry.Enums.PaymentStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class PaymentDTO {
+    private Long paymentId;
+    private PaymentStatus paymentStatus;
+    private PaymentMode paymentMode;
+
 }
