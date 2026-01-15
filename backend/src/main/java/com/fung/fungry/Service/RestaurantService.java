e8cb56cf4ee553d635f43358c07adffeb016b544
rounak <rounakgope50@gmail.com>
2026-01-15 19:03:29 +0530

    restaurant service interface added

--- backend/src/main/java/com/fung/fungry/Service/RestaurantService.java
+++ backend/src/main/java/com/fung/fungry/Service/RestaurantService.java
@@ -1,4 +1,21 @@
 package com.fung.fungry.Service;
 
+import com.fung.fungry.Model.Restaurant;
+import com.fung.fungry.ModelDTO.MenuItemDTO;
+import com.fung.fungry.ModelDTO.RestaurantDTO;
+
+import java.util.List;
+
 public interface RestaurantService {
+
+    public List<RestaurantDTO> getAllRestaurantByRatings(int page,int size,String direction,String sortBy);//pagination is neded
+    public List<MenuItemDTO> getMenuItem(Long restaurantId);
+    public RestaurantDTO addRestaurant(RestaurantDTO restaurant,Long userId);//only for admin
+    public String deleteRestaurant(Long restaurantId ,Long userId);//only for admin
+    public RestaurantDTO updateRestaurant(RestaurantDTO restaurantDTO,Long userId);
+    public RestaurantDTO rateRestaurant(Long userId,Long restaurantId,Integer rating);
+    public void addItemInMenu(MenuItemDTO itemDTO,Long restaurantId,Long userId);
+    public void deleteItemInMenu(Long menuItemId,Long restaurantId,Long userId);
+    public MenuItemDTO updateItemInMenu(Long menuItemId,Long restaurantId,Long userId);
+
 }
