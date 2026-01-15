710dca5738ee8438d3c4f8e5cc579a7b3e45e8b5
rounak <rounakgope50@gmail.com>
2026-01-15 19:42:37 +0530

    menuitemdto added

--- backend/src/main/java/com/fung/fungry/ModelDTO/MenuItemDTO.java
+++ backend/src/main/java/com/fung/fungry/ModelDTO/MenuItemDTO.java
@@ -1,4 +1,22 @@
 package com.fung.fungry.ModelDTO;
 
+import com.fung.fungry.Enums.FoodCategory;
+import com.fung.fungry.Enums.FoodType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class MenuItemDTO {
+    private Long menuItemId;
+    private FoodCategory foodCategory;
+    private FoodType foodType;
+    private String foodName;
+    private Integer availableQuantity;
+    private Double price;
+    private Boolean isAvailable;
 }
