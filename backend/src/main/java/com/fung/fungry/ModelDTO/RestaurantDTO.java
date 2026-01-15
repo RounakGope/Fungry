72fb0ab8d15786660d1155bc0a1fcefddf73f9c8
rounak <rounakgope50@gmail.com>
2026-01-15 19:33:48 +0530

    restaurant dto added

--- backend/src/main/java/com/fung/fungry/ModelDTO/RestaurantDTO.java
+++ backend/src/main/java/com/fung/fungry/ModelDTO/RestaurantDTO.java
@@ -1,4 +1,14 @@
 package com.fung.fungry.ModelDTO;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class RestaurantDTO {
+    private Long restaurantId;
+    private String name;
+    private Double rating;
 }
