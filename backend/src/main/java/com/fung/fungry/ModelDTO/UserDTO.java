0ca5f45cc4aff55f335bbdb502a0cecc229cae26
rounak <rounakgope50@gmail.com>
2026-01-16 13:47:19 +0530

    userDTo added

--- backend/src/main/java/com/fung/fungry/ModelDTO/UserDTO.java
+++ backend/src/main/java/com/fung/fungry/ModelDTO/UserDTO.java
@@ -1,4 +1,19 @@
 package com.fung.fungry.ModelDTO;
 
+import com.fung.fungry.Enums.UserRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class UserDTO {
+    private Long userId;
+    private String userName;
+    private UserRole userRole;
+    private String phoneNumber;
+
 }
