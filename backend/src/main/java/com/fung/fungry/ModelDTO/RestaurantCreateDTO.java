8f111bcdc8e7a0e28f8cc399b8857f2ec32499c8
rounak <rounakgope50@gmail.com>
2026-01-18 00:52:32 +0530

    restaurant address dto added

---
+++ backend/src/main/java/com/fung/fungry/ModelDTO/RestaurantCreateDTO.java
@@ -0,0 +1,16 @@
+package com.fung.fungry.ModelDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class RestaurantCreateDTO {
+    private Long restaurantId;
+    private String resName;
+    pr
+}
--- backend/src/main/java/com/fung/fungry/Repository/UserRepository.java
+++ backend/src/main/java/com/fung/fungry/Repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.fung.fungry.Repository;
 
+import com.fung.fungry.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository  extends JpaRepository<User,Long> {
+
 }
