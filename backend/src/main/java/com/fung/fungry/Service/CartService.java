b045a588a769d4562cc9686ad108321c8971cf12
rounak <rounakgope50@gmail.com>
2026-01-20 13:38:30 +0530

    added addtocart implementation

--- backend/src/main/java/com/fung/fungry/Service/CartService.java
+++ backend/src/main/java/com/fung/fungry/Service/CartService.java
@@ -6,7 +6,7 @@ public interface CartService {
 //no create or delete methods
     public CartDTO viewCart( Long userId);
 
-    public CartDTO addToCart(Long userId, Long menuItemId);
+    public void addToCart(Long userId, Long menuItemId);
 
     public CartDTO updateItemQuantity(Long cartItemId, Long userId, Integer quantity);
 
--- backend/src/main/java/com/fung/fungry/ServiceIMPL/CartServiceIMPL.java
+++ backend/src/main/java/com/fung/fungry/ServiceIMPL/CartServiceIMPL.java
@@ -2,13 +2,16 @@ package com.fung.fungry.ServiceIMPL;
 
 import com.fung.fungry.Model.Cart;
 import com.fung.fungry.Model.CartItem;
+import com.fung.fungry.Model.MenuItem;
 import com.fung.fungry.Model.User;
 import com.fung.fungry.ModelDTO.CartDTO;
 import com.fung.fungry.ModelDTO.CartItemDTO;
 import com.fung.fungry.Repository.CartRepository;
+import com.fung.fungry.Repository.MenuItemRepository;
 import com.fung.fungry.Repository.RestaurantRepository;
 import com.fung.fungry.Repository.UserRepository;
 import com.fung.fungry.Service.CartService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +27,8 @@ public class CartServiceIMPL implements CartService {
     CartRepository cartRepository;
     @Autowired
     RestaurantRepository restaurantRepository;
+    @Autowired
+    MenuItemRepository menuItemRepository;
     public List<CartItemDTO> cartItemDTO(List<CartItem> cartItems)
     {
         List<CartItemDTO>cartItemDTOS=new ArrayList<>();
@@ -61,10 +66,40 @@ public class CartServiceIMPL implements CartService {
     }
 
     @Override
-    public CartDTO addToCart(Long userId, Long menuItemId) {
-        return null;
-    }
+    @Transactional
+    public void addToCart(Long userId, Long menuItemId) {
 
+        User user=userRepository.findById(userId).orElseThrow(()->new RuntimeException("User Not Found"));
+        Cart cart = cartRepository.findByUser_UserId(userId).orElse(null);
+        if(cart==null)
+        {
+            cart=new Cart();
+            cart.setUser(user);
+            user.setCart(cart);
+        }
+        List<CartItem> cartItems=cart.getCartItems();
+        if(cartItems==null)
+        {
+            cart.setCartItems(new ArrayList<>());
+        }
+        MenuItem menuItem=menuItemRepository.findById(menuItemId).orElseThrow(()->new RuntimeException("No such Menu item"));
+        Optional<CartItem> existingCartItems=cart.getCartItems().stream()
+                .filter(cartItem -> cartItem.getMenuItem().getMenuItemId().equals(menuItemId))
+                .findFirst();
+        if(existingCartItems.isPresent())
+        {
+           CartItem  cartItem=existingCartItems.get();
+             cartItem.setQuantity(cartItem.getQuantity()+1);
+        }
+        else {
+            CartItem cartItem=new CartItem();
+            cartItem.setCart(cart);
+            cartItem.setQuantity(1);
+            cartItem.setMenuItem(menuItem);
+            cartItems.add(cartItem);
+        }
+        cartRepository.save(cart);
+    }
     @Override
     public CartDTO updateItemQuantity(Long cartItemId, Long userId, Integer quantity) {
         return null;
