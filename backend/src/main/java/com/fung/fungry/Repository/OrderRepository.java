5c17f9a76e4954733e1de85f3bae082be00fd190
rounak <rounakgope50@gmail.com>
2026-01-30 14:52:29 +0530

    added order HIstory dto

--- backend/src/main/java/com/fung/fungry/Repository/OrderRepository.java
+++ backend/src/main/java/com/fung/fungry/Repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.fung.fungry.Repository;
 
 import com.fung.fungry.Model.Order;
+import com.fung.fungry.Model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
+    Page<Order> findByUser(User user, Pageable pageable);
 }
