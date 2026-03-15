cfb0da57f818f30af1a07309cd9e80c2e9ff1b06
RounakGope <rounakgope50@gmail.com>
2026-03-15 20:22:35 +0530

    added add user endpoint

---
+++ backend/src/main/java/com/fung/fungry/Controller/UserController.java
@@ -0,0 +1,26 @@
+package com.fung.fungry.Controller;
+
+import com.fung.fungry.ModelDTO.UserCreateDTO;
+import com.fung.fungry.ModelDTO.UserDTO;
+import com.fung.fungry.ServiceIMPL.UserServiceIMPL;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    UserServiceIMPL userServiceIMPL;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<?> addUser(@RequestBody UserCreateDTO userCreateDTO)
+    {
+        com.fung.fungry.ModelDTO.UserDTO user =userServiceIMPL.addUser(userCreateDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+    @GetMapping("/fetchUser/{id}")
+    public ResponseEntity<?> fetchUser(@PathVariable )
+    
+}
--- backend/src/main/java/com/fung/fungry/ServiceIMPL/UserServiceIMPL.java
+++ backend/src/main/java/com/fung/fungry/ServiceIMPL/UserServiceIMPL.java
@@ -14,6 +14,7 @@ import com.fung.fungry.Repository.UserRepository;
 import com.fung.fungry.Service.UserService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
