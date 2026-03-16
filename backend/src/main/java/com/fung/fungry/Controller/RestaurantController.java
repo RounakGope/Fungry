66f5ca4f155c2527ea530d4bfc030ada13df296a
RounakGope <rounakgope50@gmail.com>
2026-03-16 20:23:47 +0530

    delete endpoint

--- backend/src/main/java/com/fung/fungry/Controller/RestaurantController.java
+++ backend/src/main/java/com/fung/fungry/Controller/RestaurantController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-v1.0")
+@RequestMapping("/api-v1.0/restaurant")
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantServiceIMPL restaurantServiceIMPL;
@@ -30,6 +30,13 @@ public class RestaurantController {
         RestaurantDTO restaurantDTO=restaurantServiceIMPL.addRestaurant(restaurantCreateDTO,userId);
         return ResponseEntity.ok(restaurantDTO);
     }
+    @DeleteMapping("/{restId}/{userId}")
+    public ResponseEntity<Void> delete(@PathVariable Long restId,@PathVariable Long userId)
+    {
+        restaurantServiceIMPL.deleteRestaurant(restId,userId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
