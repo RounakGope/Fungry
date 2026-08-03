84322aa58db7b0807f8c0b82fb516ad379a31bc0
RounakGope <rounakgope50@gmail.com>
2026-08-03 20:50:12 +0530

    security context enabled for user endpoints

--- backend/src/main/java/com/fung/fungry/Controller/UserController.java
+++ backend/src/main/java/com/fung/fungry/Controller/UserController.java
@@ -1,13 +1,20 @@
 package com.fung.fungry.Controller;
 
+import com.fung.fungry.Configuration.UserPrincipal;
 import com.fung.fungry.Enums.UserRole;
+import com.fung.fungry.Model.User;
 import com.fung.fungry.ModelDTO.*;
 import com.fung.fungry.ServiceIMPL.UserServiceIMPL;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,6 +29,8 @@ public class UserController {
         com.fung.fungry.ModelDTO.UserDTO user =userServiceIMPL.addUser(userCreateDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public ResponseEntity<List<UserDTO>> getAllUsers(
             @RequestParam(defaultValue = "0") int page,
@@ -33,49 +42,57 @@ public class UserController {
         List<UserDTO> users = userServiceIMPL.getAllUsers(page, size, dir, sort, role);
         return ResponseEntity.ok(users);
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> fetchUser(@PathVariable Long id)
+    //updated
+    @GetMapping("/fetch")
+    public ResponseEntity<UserDTO> fetchUser(@AuthenticationPrincipal UserPrincipal principal)
     {
+        Long id=principal.getUser().getUserId();
         UserDTO userDTO=userServiceIMPL.getUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body(userDTO);
     }
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id,@RequestBody UserDTO userDTO)
+    @PutMapping("/update")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO,@AuthenticationPrincipal UserPrincipal principal)
     {
-        UserDTO userDTO1=userServiceIMPL.updateUser(id, userDTO);
+        Long userId=principal.getUser().getUserId();
+        UserDTO userDTO1=userServiceIMPL.updateUser(userId, userDTO);
         return ResponseEntity.ok(userDTO1);
     }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id)
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> delete(@AuthenticationPrincipal UserPrincipal principal)
     {
+        Long id=principal.getUser().getUserId();
         userServiceIMPL.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
-    @GetMapping("/userRole/{id}")
-    public ResponseEntity<UserRole> userRole(@PathVariable Long id)
+    @GetMapping("/userRole")
+    public ResponseEntity<UserRole> userRole(@AuthenticationPrincipal UserPrincipal principal)
     {
+        Long id=principal.getUser().getUserId();
         UserRole userRole=userServiceIMPL.getUserRole(id);
         return ResponseEntity.ok(userRole);
     }
-    @PutMapping("/updatePhone/{id}")
-    public ResponseEntity<UserDTO> updatePhone(@PathVariable Long id,@RequestBody PhoneDTO Number)
+    @PutMapping("/updatePhone")
+    public ResponseEntity<UserDTO> updatePhone(@RequestBody PhoneDTO Number,@AuthenticationPrincipal UserPrincipal principal)
     {
+        Long id=principal.getUser().getUserId();
         UserDTO userDTO=userServiceIMPL.updateUserPNo(id,Number.getNumber());
         return ResponseEntity.ok(userDTO);
     }
-    @PutMapping("/updatePassword/{id}")
-    public ResponseEntity<UserDTO> updatePass(@PathVariable Long id, @RequestBody PasswordUpdateDTO passwordUpdateDTO)
+    @PutMapping("/updatePassword")
+    public ResponseEntity<UserDTO> updatePass( @RequestBody PasswordUpdateDTO passwordUpdateDTO,@AuthenticationPrincipal UserPrincipal principal)
     {
+
+        Long id=principal.getUser().getUserId();
         UserDTO userDTO=userServiceIMPL.updatePassword(id,
                 passwordUpdateDTO.getOldPassword(), passwordUpdateDTO.getNewPassword());
         return ResponseEntity.ok(userDTO);
     }
-    @GetMapping("/orderHistory/{id}")
-    public ResponseEntity<List<OrderHistoryDTO>> orderHistory(@PathVariable Long id
-    , @RequestParam (defaultValue = "0")Integer page,@RequestParam
+    @GetMapping("/orderHistory")
+    public ResponseEntity<List<OrderHistoryDTO>> orderHistory(@RequestParam (defaultValue = "0")Integer page,@RequestParam
                                                                           (defaultValue = "15")Integer size, @RequestParam (defaultValue = "createdAt")String sortBy,
-                                                              @RequestParam (defaultValue = "descending")String direction)
+                                                              @RequestParam (defaultValue = "descending")String direction, @AuthenticationPrincipal UserPrincipal principal)
     {
+        Long id=principal.getUser().getUserId();
         List<OrderHistoryDTO> dtoList=userServiceIMPL.viewOrderHistory
                 (id,page,size,sortBy,direction);
         return ResponseEntity.ok(dtoList);
